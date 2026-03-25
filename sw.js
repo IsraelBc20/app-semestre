fab624d99740e3e829a6e2f0ef1a7f6c754f5914
IsraelBc20 <81199659+IsraelBc20@users.noreply.github.com>
2026-03-25 18:57:22 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'app-calendario-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Instala el Service Worker y guarda los archivos básicos
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Intercepta las peticiones de red para usar la memoria caché si estás offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Devuelve la versión guardada
+        }
+        return fetch(event.request); // Si no está guardado, lo busca en internet
+      })
+  );
+});
